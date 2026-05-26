760fc40064756bbe3cc3e74246b514e55b59ac94
Guilherme Branco <guiguiguiduarte@gmail.com>
2026-05-26 09:31:00 +0100

    first commit

---
+++ WeatherAppForms/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WeatherAppForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.searchbtn = new System.Windows.Forms.Button();
+            this.citytxt = new System.Windows.Forms.TextBox();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.weatherIcon = new System.Windows.Forms.PictureBox();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.weatherIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchbtn
+            // 
+            this.searchbtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(138)))), ((int)(((byte)(221)))));
+            this.searchbtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.searchbtn.FlatAppearance.BorderSize = 0;
+            this.searchbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.searchbtn.ForeColor = System.Drawing.Color.White;
+            this.searchbtn.Location = new System.Drawing.Point(316, 33);
+            this.searchbtn.Name = "searchbtn";
+            this.searchbtn.Size = new System.Drawing.Size(42, 42);
+            this.searchbtn.TabIndex = 0;
+            this.searchbtn.Text = "→";
+            this.searchbtn.UseVisualStyleBackColor = false;
+            this.searchbtn.Click += new System.EventHandler(this.searchbtn_Click);
+            // 
+            // citytxt
+            // 
+            this.citytxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(34)))), ((int)(((byte)(64)))));
+            this.citytxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.citytxt.Cursor = System.Windows.Forms.Cursors.Default;
+            this.citytxt.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.citytxt.ForeColor = System.Drawing.Color.White;
+            this.citytxt.Location = new System.Drawing.Point(12, 39);
+            this.citytxt.Name = "citytxt";
+            this.citytxt.Size = new System.Drawing.Size(240, 24);
+            this.citytxt.TabIndex = 1;
+            this.citytxt.Text = "City";
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Font = new System.Drawing.Font("Segoe UI", 52.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemp.ForeColor = System.Drawing.Color.White;
+            this.lblTemp.Location = new System.Drawing.Point(80, 231);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(278, 116);
+            this.lblTemp.TabIndex = 2;
+            this.lblTemp.Text = "label1";
+            this.lblTemp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // weatherIcon
+            // 
+            this.weatherIcon.BackColor = System.Drawing.Color.Transparent;
+            this.weatherIcon.Location = new System.Drawing.Point(172, 103);
+            this.weatherIcon.Name = "weatherIcon";
+            this.weatherIcon.Size = new System.Drawing.Size(80, 80);
+            this.weatherIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.weatherIcon.TabIndex = 3;
+            this.weatherIcon.TabStop = false;
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.BackColor = System.Drawing.Color.Transparent;
+            this.lblCity.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.ForeColor = System.Drawing.Color.White;
+            this.lblCity.Location = new System.Drawing.Point(171, 196);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(81, 35);
+            this.lblCity.TabIndex = 5;
+            this.lblCity.Text = "label2";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(185, 455);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 16);
+            this.lblError.TabIndex = 6;
+            this.lblError.Visible = false;
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(188, 372);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(44, 16);
+            this.lblDesc.TabIndex = 7;
+            this.lblDesc.Text = "label1";
+            this.lblDesc.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 508);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.weatherIcon);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.citytxt);
+            this.Controls.Add(this.searchbtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Weather Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.weatherIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button searchbtn;
+        private System.Windows.Forms.TextBox citytxt;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.PictureBox weatherIcon;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label lblDesc;
+    }
+}
+
---
+++ WeatherAppForms/Form1.cs
@@ -0,0 +1,180 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherAppForms
+{
+
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            //form setup
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.FromArgb(15, 20, 40);  
+            this.Size = new Size(420, 580);
+            this.Font = new Font("Segoe UI", 9f);
+            this.ForeColor = Color.White;
+
+            
+            Panel pnlSearch = new Panel();
+            pnlSearch.Size = new Size(378, 48);
+            pnlSearch.Location = new Point(20, 20);
+            pnlSearch.BackColor = Color.FromArgb(22, 29, 55);
+            this.Controls.Add(pnlSearch);
+            this.Controls.SetChildIndex(pnlSearch, 0);
+
+            // textbox
+            citytxt.Parent = pnlSearch;
+            citytxt.Location = new Point(16, 14);
+            citytxt.Size = new Size(290, 22);
+            citytxt.BackColor = Color.FromArgb(22, 29, 55);  
+            citytxt.ForeColor = Color.FromArgb(200, 210, 230);
+            citytxt.Font = new Font("Segoe UI", 11f);
+            citytxt.BorderStyle = BorderStyle.None;
+
+            // btn
+            searchbtn.Parent = pnlSearch;
+            searchbtn.Location = new Point(328, 3);
+            searchbtn.Size = new Size(42, 42);
+            searchbtn.Text = "→";
+            searchbtn.FlatStyle = FlatStyle.Flat;
+            searchbtn.FlatAppearance.BorderSize = 0;
+            searchbtn.ForeColor = Color.White;
+            searchbtn.Cursor = Cursors.Hand;
+            searchbtn.Font = new Font("Segoe UI", 14f);
+            btnstyle(searchbtn,
+                Color.FromArgb(55, 138, 221),
+                Color.FromArgb(42, 109, 191));
+
+            // icon config
+            weatherIcon.Location = new Point(170, 100);
+            weatherIcon.Size = new Size(80, 80);
+            weatherIcon.BackColor = Color.Transparent;
+            weatherIcon.SizeMode = PictureBoxSizeMode.Zoom;
+
+            //labels
+            lblCity.BackColor = Color.Transparent;
+            lblCity.ForeColor = Color.FromArgb(140, 155, 180);
+            lblCity.Font = new Font("Segoe UI", 11f);
+            lblCity.Text = "";
+            lblCity.AutoSize = true;
+            lblCity.Location = new Point(0, 190);  
+            lblCity.Anchor = AnchorStyles.None;
+
+            lblTemp.BackColor = Color.Transparent;
+            lblTemp.ForeColor = Color.White;
+            lblTemp.Font = new Font("Segoe UI Light", 64f);
+            lblTemp.Text = "";
+            lblTemp.AutoSize = true;
+            lblTemp.Location = new Point(0, 230);
+
+            lblDesc.BackColor = Color.Transparent;
+            lblDesc.ForeColor = Color.FromArgb(140, 155, 180);
+            lblDesc.Font = new Font("Segoe UI", 11f);
+            lblDesc.Text = "";
+            lblDesc.AutoSize = true;
+
+            // cent labels
+            this.Resize += (s, e) => CentLabels();
+            this.Load += (s, e) => CentLabels();
+
+            // error
+            lblError.BackColor = Color.Transparent;
+            lblError.ForeColor = Color.FromArgb(255, 100, 100);
+            lblError.Visible = false;
+            lblError.Location = new Point(20, 490);
+
+            // search func
+            citytxt.KeyDown += (s, e) => {
+                if (e.KeyCode == Keys.Enter) searchbtn_Click(s, e);
+            };
+        }
+        private void CentLabels()
+        {
+            lblCity.Left = (this.ClientSize.Width - lblCity.Width) / 2;
+            lblTemp.Left = (this.ClientSize.Width - lblTemp.Width) / 2;
+            lblDesc.Left = (this.ClientSize.Width - lblDesc.Width) / 2;
+            weatherIcon.Left = (this.ClientSize.Width - weatherIcon.Width) / 2;
+        }
+        private void btnstyle(Button btn, 
+            System.Drawing.Color normalcolor,
+            System.Drawing.Color hovercolor)
+        {
+            btn.BackColor = normalcolor;
+            btn.MouseEnter += (s, e) => { btn.BackColor = hovercolor; };
+            btn.MouseLeave += (s, e) => { btn.BackColor = normalcolor; };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void searchbtn_Click(object sender, EventArgs e)
+        {
+            string city = citytxt.Text.Trim();
+            if (string.IsNullOrEmpty(city)) return;    
+
+            //loadind
+            searchbtn.Enabled = false;
+            searchbtn.Text = "...";
+
+            try
+            {
+                string apiKey = "API_KEY_HERE";
+                string url = $"https://api.openweathermap.org/data/2.5/weather" +
+                                $"?q={city}&appid={apiKey}&units=metric&lang=eng";
+
+
+                //data save
+                string json = await client.GetStringAsync(url);
+                var data = Newtonsoft.Json.Linq.JObject.Parse(json);
+
+
+                //label update
+                lblDesc.Text = data["weather"][0]["description"].ToString();
+                lblCity.Text = $"{data["name"]}, {data["sys"]["country"]}";
+                lblTemp.Text = $"{Math.Round((double)data["main"]["temp"])}º";
+                CentLabels();
+
+
+                //icons
+                string icon = data["weather"][0]["icon"].ToString();
+                string iconUrl = $"https://openweathermap.org/img/wn/{icon}@2x.png";
+                weatherIcon.Load(iconUrl);
+                lblError.Visible = false;
+            }
+            catch (HttpRequestException)
+            {
+                lblError.Text = $"City not found";
+                lblError.Visible = true;
+            }
+            finally
+            {
+                searchbtn.Enabled = true;
+                searchbtn.Text = "→";
+            }
+
+        }
+        private void Form1_load(object sender, EventArgs e) { }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
